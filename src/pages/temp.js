d848532e54417e649405c5684fec8101c7c36292
abdulmoiz14 <65711565+abdulmoiz14@users.noreply.github.com>
2023-05-25 02:40:44 +0500

    configuration

---
+++ src/pages/temp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CollapsibleExample from "../Components/Navbar.js";
+function Home(){
+    return (
+        <>
+            <CollapsibleExample />
+        </>
+    );
+}
+export default Home;
